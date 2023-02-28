d24316d42c95e882584b0ee6e3e261c008d2adec
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2023-03-01 00:55:24 +0100

    fix 0

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import './fonts/MarketSans-Bold.ttf';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <BrowserRouter>
+   <HashRouter>
     <App />
-   </BrowserRouter>
+   </HashRouter>
   </React.StrictMode>
 );
